43f5a1727b6098d3b4d958b13cb392e1aa87bd3a
Gayathri Gireesh Sujatha <gayathri.sujatha@arup.com>
2024-08-30 18:04:11 +0530

    base code

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 37;
+            double pi = 3.14;
+            string name = "Varu mon";
+            bool isStudent = true;
+
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Pi: " + pi);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Is Student: " + isStudent);
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# csharp-hello-world
